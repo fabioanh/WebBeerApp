f84fa5ecebffae3417bf0c1962c97c8d5a2c9a17
NAVARRETE Fabio (ext) <u510290@bpost.be>
2016-11-14 15:22:19 +0000

    changes required in the schema

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,21 +1,17 @@
-CREATE TABLE beer (
-  id         INTEGER PRIMARY KEY,
-  name VARCHAR(200),
-  type  VARCHAR(100),
-  alcohol_content DOUBLE,
-  brewery_id INTEGER
-);
-
 CREATE TABLE brewery(
   id INTEGER PRIMARY KEY,
   name VARCHAR (200),
   address VARCHAR (300)
 );
 
-
-ALTER TABLE beer
-ADD FOREIGN KEY (brewery_id) 
-REFERENCES brewery(id);
+CREATE TABLE beer (
+  id         INTEGER PRIMARY KEY,
+  name VARCHAR(200),
+  type  VARCHAR(100),
+  alcohol_content DOUBLE,
+  brewery_id INTEGER,
+  constraint fk_brewery foreign key(brewery_id) references brewery(id)
+);
 
 
 -- SECURITY SCHEMA CREATION
