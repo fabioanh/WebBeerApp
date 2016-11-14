8b25a64fddcf232046d34ffad960eedd2219d5d1
NAVARRETE Fabio (ext) <u510290@bpost.be>
2016-11-14 16:02:14 +0000

    Initial spring security configuration using in memory authentication

---
+++ src/main/java/com/adneom/WebSecurityConfig.java
@@ -0,0 +1,36 @@
+package com.adneom;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		//@formatter:off
+        http
+            .authorizeRequests()
+                .antMatchers("/", "/beers").permitAll()
+                .anyRequest().authenticated()
+                .and()
+            .formLogin()
+                .permitAll()
+                .and()
+            .logout()
+                .permitAll();
+        //@formatter:on
+	}
+	
+	@Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+            .inMemoryAuthentication()
+                .withUser("user").password("password").roles("USER");
+    }
+}
